0c0ab5db8e741dface50b0f9fb136c0c363b20d3
Kelly-Fiama <91966374+Kelly-Fiama@users.noreply.github.com>
2021-11-30 14:02:51 -0300

    Desafio da Catapulta

--- Program.cs
+++ Program.cs
@@ -6,92 +6,71 @@ namespace dotnetcore
     {
         static void Main(string[] args)
         {
-            // Console.WriteLine -> escreve algo na tela
-
-            /*
-            várias
-            linhas
-            */
-
-            /*
-            // Variáveis -> Guardam na memória RAM
-            // 1ª - tipo da informação (em alguns casos, podemos substituir por 'var')
-            // 2ª - o que aquela informação representa
-            // 3ª - a informação em si
-            */
-
-            // instrução finaliza com ';'
-            //String - "aspas duplas"
-
+            /* Catapulta 
+    
+            Recebe:     o número de baterias (n) e a duração da bateria(d)
+            Calcula:    o número de pedras (q) que a catapulta irá soltar
             
-            //Exemplo 1            
-            /*
-            string nome = "Meu Nome";
-            Console.WriteLine(nome);
-            */
-
-            // Exemplo 2
-
-            /*
-            string nome = "Nome";
-            string sobrenome = "Sobrenome";
+            Assim, q = f(n,d), em que f é uma função desconhecida qualquer calculada em 
+            função de n e d.
             
-            Console.WriteLine(nome + " " + sobrenome);
-            */
-
-            // Exemplo 3
-            // Formatação de strings
-
-            /*
-            string nome = "Nome";
-            string sobrenome = "Sobrenome";
-            string nomeCompleto = $"Olá, {nome} {sobrenome}!";
-            Console.WriteLine(nomeCompleto);
-            */
-
-            // Exemplo 4
-            /*
-            Console.WriteLine("Digite o seu nome:");
-            string nome = Console.ReadLine();
-            Console.WriteLine($"Olá, {nome}!");
-            */
-            // Exemplo 5
-
-            var nome = "Nome";
-            var idade = 00;
-            var altura = 0.0;
-
-            Console.WriteLine($"$Olá, {nome}, você possui {idade} anos de idade e mede {altura}cm.");
-
-            var soma = 10 + 5;
-
-            Console.WriteLine($"10 + 5 = {soma}");
-
+            Considere que a catapulta funcione com uma bateria por vez e que seja 
+            desprezível o tempo entre trocas de baterias. Seja t o tempo total de 
+            funcionamento da catapulta, isto é, t = n * d. Assim, q = f(n, d) = f(t)
             
-
-            // Desafio 1
-            // Calculadora com 10% do garçon
-            // Valor da conta, valor da taxa adicional, resultado
-
-            var valorConta = 45.35;
-            var taxaGarcon = 0.1 * valorConta;
-            var contaTotal = valorConta + taxaGarcon;
-
-            Console.WriteLine($"Valor: R{valorConta:C}, Taxa: R{taxaGarcon:C}, Total: {contaTotal:C}.");
-
+            Considere também que o lançamento de pedra pela catapulta seja diretamente
+            proporcional ao tempo de funcionamento dela. Assim, q = f(t) = alfa * t, em 
+            que alfa é uma constante de operação da catapulta, ou seja, a taxa do número 
+            de pedras lançadas por minuto.
+            
+            Nota - Análise Dimensional: 
+                [q]    = número de pedras
+                [t]    = minutos
+                
+                Se [q] = [alfa] *[t], então [alfa] = [q]/[t] = número de pedras/ minutos
+            
+            A taxa alfa pode ser dada por qualquer relação q/t. Adote que n0, d0 e q0 
+            sejam informações iniciais que se tem a sobre o funcionamento da catapulta.
+            
+            Assim,
+            
+            alfa = q0/t0 = q0 / (n0 * d0)
+            
+            Sabemos, a priori, que a catapulta lançou 300 pedras (q0) com 5 baterias (n0) 
+            com duração 15 minutos cada (d0). Logo, alfa = 300 / (5 * 15) = 4 pedras/min
+            
+            Portanto,
+            
+            q = alfa * t = 4 * t = 4 * n * d
+            */
+            
+            var alfa = 4;
+            
+            Console.WriteLine("Digite o número de baterias (n): ");
+            //var nString = Console.ReadLine();
+            var n = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"n = {n}");
+            
+            Console.WriteLine("Digite a duração da bateria (d): ");
+            //var nString = Console.ReadLine();
+            var d = int.Parse(Console.ReadLine());
+            //Console.WriteLine($"d = {d}");
+            
+            Console.WriteLine($"A catapulta lançará {alfa * n * d} com {n} baterias de {d} minutos de duração cada");
+            
             /*
-            Desafio para depois:
-
-            Nome: Catapulta
-            Objetivo: Receber o número de baterias e duração da bateria e calcular a quantidade de pedras que a catapulta irá soltar.
-            Dificuldade: Principiante
-
-            1 - Uma catapulta lançou 300 pedras em 5 baterias de 15 minutos, cada.
-
-            2 - Quantas pedras ela lançaria em 8 baterias de 7 minutos, cada?
-
-            3 - Crie um programa que receba os valores base para que a aplicação funcione de forma que, se alterarmos o número de baterias e a duração de cada bateria, o programa funcione sem precisar de mais modificações.
-
+            Caso queira trocar as configuração da catapulta, troque var alfa = 4; por:
+            
+            Console.WriteLine("Digite o número de pedras lançadas (q0): ");
+            var n0 = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Digite o número de baterias (n0): ");
+            var n0 = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine("Digite a duração da bateria (d0): ");
+            var d0 = int.Parse(Console.ReadLine());
+            
+            alfa = q0 / (n0 * d0);
             */
         }
     }
